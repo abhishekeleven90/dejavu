74e556983161425c45ad946249976aa630a2f8b7
Harinder Sethi <sethi.harinder@gmail.com>
2014-09-28 02:19:12 +0530

    tmp

--- MyThread/src/temp_h.cpp
+++ MyThread/src/temp_h.cpp
@@ -2,16 +2,17 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
+#include <math.h>
 using namespace std;
 /*
-#include <string.h>
-#include <iostream>
-#include <stdio.h>
+ #include <string.h>
+ #include <iostream>
+ #include <stdio.h>
 
-#include <stdlib.h>
-using namespace std;
+ #include <stdlib.h>
+ using namespace std;
 
-#include "MyThread.h"
+ #include "MyThread.h"
  #include <iostream>
  #define SECOND 1000000
 
@@ -57,33 +58,32 @@ using namespace std;
 
 
 
-int main() {
-	while (1) {
-		char ui_data[1024];
-		fgets(ui_data, sizeof(ui_data), stdin);
-		cout << ui_data << endl;
-		cout << "outside" << endl;
-		if (strcmp(ui_data, "create\n") == 0) {
-			cout << "inside";
-		}
-	}
-	return 0;
-}
+ int main() {
+ while (1) {
+ char ui_data[1024];
+ fgets(ui_data, sizeof(ui_data), stdin);
+ cout << ui_data << endl;
+ cout << "outside" << endl;
+ if (strcmp(ui_data, "create\n") == 0) {
+ cout << "inside";
+ }
+ }
+ return 0;
+ }
 
-*/
-
+ */
 
 int main() {
 	int m = 160;
-	char token[] = "0000000000000000000000000000000000000000";
 	int index = 39;
-	for (int i = 1; i < m; i++) {
-		int tmp = (i % 4) * 2;
-		token[index] = (char)(tmp+48);
-		cout << token << endl;
-		if (i % 4 == 0) {
+	for (int i = 0; i < m; i++) {
+		if (i != 0 && i % 4 == 0) {
 			index--;
 		}
+		char token[] = "0000000000000000000000000000000000000000";
+		int tmp = pow(2, i % 4);
+		token[index] = (char) (tmp + 48);
+		cout << "i: " << i << " " << token << endl;
 	}
 	return 0;
 }
